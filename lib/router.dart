e12b4c8e14b55189e50e5eaa832ed349791e6475
Aiman Zaki <aiman0zaki@gmail.com>
2019-07-28 21:21:15 +0800

    bloc routing

--- lib/router.dart
+++ lib/router.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:peeps/bloc/bloc.dart';
+import 'package:peeps/resources/groupwork_repository.dart';
+import 'package:peeps/resources/users_repository.dart';
+import 'package:peeps/screens/inbox.dart';
+import 'package:peeps/screens/inbox_bottombar.dart';
 import 'package:peeps/screens/joined_group.dart';
 import 'package:peeps/screens/account.dart';
 import 'package:peeps/screens/profile.dart';
@@ -12,7 +18,21 @@ Route<dynamic> generateRoute(RouteSettings settings){
     case AccountViewRoute:
       return CupertinoPageRoute(builder: (context) => AccountView());
     case GroupsViewRoute:
-      return CupertinoPageRoute(builder: (context) => GroupsView());
+      return CupertinoPageRoute<GroupsView>(
+                builder: (context) {
+                  return BlocProvider<GroupworkBloc>.value(
+                    value: GroupworkBloc(repository: GroupworkRepository()),
+                    child: GroupsView(),
+                  );
+                }
+              );
+    case InboxBottomBarViewRoute:
+      return CupertinoPageRoute(builder: (context) {
+        return BlocProvider<InboxBloc>.value(
+          value: InboxBloc(repository: UsersRepository()),
+          child: InboxBottomBarView(),
+        );
+      });
     default:
       return MaterialPageRoute(builder: (context) => HomeView());
   }
--- lib/routing_constant.dart
+++ lib/routing_constant.dart
@@ -2,3 +2,5 @@ const String HomeViewRoute = '/';
 const String AccountViewRoute = 'account';
 const String ProfileViewRoute = 'profile';
 const String GroupsViewRoute = 'groups';
+const String InboxViewRoute = 'inbox';
+const String InboxBottomBarViewRoute = 'inbox_bottomBar';
