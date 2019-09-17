d2aeea70a98ede3ca5bb4c5249cbc3e5a92bc08a
Aiman Zaki <aiman0zaki@gmail.com>
2019-09-17 20:07:28 +0800

    Update router with navigation args

--- lib/router.dart
+++ lib/router.dart
@@ -4,6 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:peeps/bloc/bloc.dart';
 import 'package:peeps/resources/groupwork_repository.dart';
 import 'package:peeps/resources/users_repository.dart';
+import 'package:peeps/router/navigator_args.dart';
 import 'package:peeps/screens/inbox_invitation.dart';
 import 'package:peeps/screens/inbox_bottombar.dart';
 import 'package:peeps/screens/joined_group.dart';
@@ -17,23 +18,34 @@ Route<dynamic> generateRoute(RouteSettings settings){
     case HomeViewRoute:
       return CupertinoPageRoute(builder: (context) => HomeView());
     case AccountViewRoute:
-      return CupertinoPageRoute(builder: (context) => AccountView(data: settings.arguments,));
+      
+      final NavigatorArguments args = settings.arguments;
+
+      return CupertinoPageRoute(builder: (context) => 
+        BlocProvider(
+            builder: (context) => ProfileFormBloc(repository: const UsersRepository(), bloc: args.bloc),
+              child: AccountView(data: args.data,)));
+    
     case GroupsViewRoute:
+      final NavigatorArguments args = settings.arguments;
+
       return CupertinoPageRoute<GroupworksView>(
                 builder: (context) {
                   return BlocProvider<GroupworkBloc>.value(
                     value: GroupworkBloc(repository: GroupworkRepository()),
-                    child: GroupworksView(user:settings.arguments),
+                    child: GroupworksView(user: args.data),
                   );
                 }
               );
+    
     case InboxBottomBarViewRoute:
       return CupertinoPageRoute(builder: (context) {
         return BlocProvider<InboxBloc>.value(
-          value: InboxBloc(repository: UsersRepository(),profileBloc: ProfileBloc(repository: UsersRepository())),
+          value: InboxBloc(repository: UsersRepository(),profileBloc: ProfileBloc(repository: const UsersRepository())),
           child: InboxBottomBarView(),
         );
       });
+      
     case SearchViewRoute:
       return CupertinoPageRoute(builder: (context){
         return SearchView();
---
+++ lib/router/navigator_args.dart
@@ -0,0 +1,9 @@
+import 'package:meta/meta.dart';
+class NavigatorArguments{
+  final data;
+  final bloc;
+  NavigatorArguments({
+    @required this.data,
+    @required this.bloc
+  });
+}
