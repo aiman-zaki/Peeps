216084bcb22da5f12fbc9895e861afea02f09487
aimanzaki <aiman0zaki@gmail.com>
2019-10-09 17:30:11 +0800

    Add more local notifications function

--- lib/configs/notifications.dart
+++ lib/configs/notifications.dart
@@ -1,10 +1,27 @@
 import 'dart:typed_data';
 import 'dart:ui';
 
+import 'package:flutter/rendering.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class LocalNotifications {
+
+  Future<int> _notificationsId()  async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    int ids = preferences.getInt("tasks");
+    if(ids == null){
+      ids = 0;
+    }
+    return ids;
+  }
+
+  _incrementId(id) async{
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    preferences.setInt("tasks", (id+1));
+    
+  }
+
   Future<void> showNotification(flutterLocalNotificationsPlugin) async {
     var androidPlatformChannelSpecifics = AndroidNotificationDetails(
         'your channel id', 'your channel name', 'your channel description',
@@ -18,15 +35,10 @@ class LocalNotifications {
   }
 
   Future<void> scheduleNotification(
-      flutterLocalNotificationsPlugin, DateTime datetime,String taskId) async {
-    
-    SharedPreferences _preferences = await SharedPreferences.getInstance();
-    var data = _preferences.getBool('task_$taskId');
-    
-    
+    flutterLocalNotificationsPlugin, DateTime datetime,task) async {
     
     var scheduledNotificationDateTime =
-      datetime.subtract(new Duration(days: 1));
+      datetime;
     var vibrationPattern = Int64List(4);
     vibrationPattern[0] = 0;
     vibrationPattern[1] = 1000;
@@ -34,16 +46,30 @@ class LocalNotifications {
     vibrationPattern[3] = 2000;
 
     var androidPlatformChannelSpecifics = AndroidNotificationDetails(
-        'your channel id', 'your channel name', 'your channel description',
+        '', 'Peeps', 'your channel description',
         importance: Importance.Max, priority: Priority.High, ticker: 'ticker');
     var iOSPlatformChannelSpecifics = IOSNotificationDetails();
     var platformChannelSpecifics = NotificationDetails(
         androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
     await flutterLocalNotificationsPlugin.schedule(
-      0,
-      'scheduled title',
-      'scheduled body',
+      task.seq,
+      'Task : ${task.task}',
+      'Due Date : ${task.dueDate}',
       scheduledNotificationDateTime,
       platformChannelSpecifics);
   }
+
+
+  Future<void> checkPendingNotificationRequests(flutterLocalNotificationsPlugin) async {
+    var pendingNotificationRequests =
+        await flutterLocalNotificationsPlugin.pendingNotificationRequests();
+
+        
+    for (var pendingNotificationRequest in pendingNotificationRequests) {
+      debugPrint(
+          'pending notification: [id: ${pendingNotificationRequest.id}, title: ${pendingNotificationRequest.title}, body: ${pendingNotificationRequest.body}, payload: ${pendingNotificationRequest.payload}]');
+    }
+  }
 }
+
+  
